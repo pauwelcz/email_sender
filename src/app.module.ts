d211816f5e4f4070c911e1087d8458fa65749c35
pauwelcz <pavel.sedlar1993@gmail.com>
2024-05-16 23:14:34 +0200

    Winston logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,8 @@ import { AppService } from './app.service';
 import { EmailModule } from './email/email.module';
 import { BullModule } from '@nestjs/bullmq';
 import { AuthModule } from './auth/auth.module';
+import { WinstonModule } from 'nest-winston';
+import * as winston from 'winston';
 
 @Module({
   imports: [
@@ -12,6 +14,14 @@ import { AuthModule } from './auth/auth.module';
         host: 'redis',
         port: 6379,
       },
+    }),
+    WinstonModule.forRoot({
+      transports: [
+        new winston.transports.Console(),
+        new winston.transports.File({
+          filename: 'logs/requests.log',
+        }),
+      ],
     }),
     EmailModule,
     AuthModule,
--- src/email/email.controller.ts
+++ src/email/email.controller.ts
@@ -5,18 +5,25 @@ import {
   HttpStatus,
   Post,
   UseGuards,
+  Inject,
 } from '@nestjs/common';
 import { EmailService } from './email.service';
 import { SendEmailDto } from './dto/email.dto';
 import { AuthGuard } from '../auth/auth.guard';
+import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
+import { Logger } from 'winston';
 
 @Controller('email')
 export class EmailController {
-  constructor(private readonly emailsService: EmailService) {}
+  constructor(
+    private readonly emailsService: EmailService,
+    @Inject(WINSTON_MODULE_PROVIDER) private readonly logger: Logger,
+  ) {}
 
   @UseGuards(AuthGuard)
   @Post()
   async sendEmail(@Body() sendEmailDto: SendEmailDto) {
+    this.logger.info(`${new Date()} - Request received`);
     const delay = await this.emailsService.sendEmail(sendEmailDto);
 
     if (delay === 0) throw new HttpException('OK', HttpStatus.OK);
